70cebed08ec20bdd041ab75d28f87c88003e0998
Thejan Rajapakshe <coder.clix@gmail.com>
2015-09-04 06:33:19 +0530

    init

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header('application/json');
+header('Content-Type: application/json');
 $array = array('id'=>1,'name'=>'Thejan Rajapakshe','email'=>"coder.clix@gmail.com");
 echo json_encode($array);
     ?>
